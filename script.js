2f3ea13990f1a6e470567d7b3a6510f307bff713
Ashish2106B <ashish21bisht@gmail.com>
2026-04-07 10:01:48 +0530

    added the search functionality

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let articles = []
+
 let API_KEY = "9854af16b847e1da9ff7504a636c5a02"
 
 let URL = `https://gnews.io/api/v4/top-headlines?lang=en&token=${API_KEY}`
@@ -12,19 +14,58 @@ fetch(URL)
 .then(data => {
     loading.style.display = "none"
 
-    let articles = data.articles
+    articles = data.articles   
 
-    articles.forEach(a => {
-        let div = document.createElement("div")
-
-        div.innerHTML = `
-            <h3>${a.title}</h3>
-            <p>${a.description || ""}</p>
-        `
-
-        container.appendChild(div)
-    })
+    showNews(articles)         
 })
 .catch(() => {
     loading.innerText = "Failed to load news"
 })
+
+
+
+function showNews(arr){
+    container.innerHTML = ""
+
+    arr.forEach(a => {
+        let card = document.createElement("div")
+        card.className = "card"
+
+        card.innerHTML = `
+            <img src="${a.image || 'https://via.placeholder.com/300'}">
+            <h3>${a.title}</h3>
+            <p>${a.description || ""}</p>
+            <a href="${a.url}" target="_blank">Read More</a>
+        `
+
+        container.appendChild(card)
+    })
+}
+
+
+
+function searchNews(){
+    let query = document.getElementById("search").value
+
+    let url = `https://gnews.io/api/v4/search?q=${query}&lang=en&token=${API_KEY}`
+
+    loading.style.display = "block"
+
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        loading.style.display = "none"
+
+        articles = data.articles
+
+        showNews(articles)
+    })
+    .catch(() => {
+        loading.innerText = "Failed to load news"
+    })
+}
+
+
+
+
+
